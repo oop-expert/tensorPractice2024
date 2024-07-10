b167e2918f3479a2d0eb0e91f11d4afba18703b0
elenaz441 <ezasypkina03@list.ru>
2024-07-10 22:35:38 +0500

    Add tests

--- questions/urls.py
+++ questions/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import QuestionAPIView
 
 urlpatterns = [
-    path('', QuestionAPIView.as_view()),
+    path('', QuestionAPIView.as_view(), name='questions'),
 ]
--- questions/views.py
+++ questions/views.py
@@ -1,16 +1,23 @@
-from django.shortcuts import render
 from rest_framework import generics
 from .models import Question
 from .serializers import QuestionSerializer
+from rest_framework import status
+from rest_framework.response import Response
 
 
 class QuestionAPIView(generics.ListAPIView):
     queryset = Question.objects.all()
     serializer_class = QuestionSerializer
 
-    def get_queryset(self):
-        count = self.request.query_params.get('count')
-        if not count or int(count) < 5 or int(count) > 100:
+    def get(self, request, *args, **kwargs):
+        count = request.query_params.get('count')
+        if count:
+            if not count.isdigit():
+                return Response({'message': 'count must be an integer'}, status=status.HTTP_400_BAD_REQUEST)
+            count = int(count)
+            if not 5 <= count <= 100:
+                return Response({'message': 'count must be between 5 and 100'}, status=status.HTTP_400_BAD_REQUEST)
+        else:
             count = 10
-        queryset = Question.objects.all().order_by('?')[:int(count)]
-        return queryset
+        questions = QuestionSerializer(Question.objects.all().order_by('?')[:count], many=True).data
+        return Response(questions)
