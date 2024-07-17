9008db7f2950847c3c00189078f5460890d8cfcd
SpOOn <60212126+Spoon221@users.noreply.github.com>
2024-07-17 18:01:11 +0500

    qr code

--- api/models.py
+++ api/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 import string
 import random
-
+import qrcode
+import base64
+import io
 
 def generate_unique_code():
     length = 6
@@ -13,6 +15,14 @@ def generate_unique_code():
 
     return code
 
+def generate_qr_code(code):
+    qr = qrcode.QRCode(version=1, box_size=10, border=5)
+    qr.add_data(code)
+    qr.make(fit=True)
+    #img = qr.make_image(fill_color="black", back_color="white")
+    img_bytes = io.BytesIO()
+    #img.save(img_bytes, format='PNG')
+    return base64.b64encode(img_bytes.getvalue()).decode('utf-8')
 
 class Room(models.Model):
     code = models.CharField(max_length=8, default=generate_unique_code, unique=True)
@@ -20,3 +30,9 @@ class Room(models.Model):
     guest_can_pause = models.BooleanField(null=False, default=False)
     limit_of_players = models.IntegerField(null=False, default=1)
     created_at = models.DateTimeField(auto_now_add=True)
+    qr_code_base64 = models.CharField(max_length=1000, blank=True)  # Изменил имя поля
+
+    def save(self, *args, **kwargs):
+        if not self.qr_code_base64:
+            self.qr_code_base64 = generate_qr_code(self.code)  # Сгенерировать QR-код
+        super().save(*args, **kwargs)  # Сохранить модель
