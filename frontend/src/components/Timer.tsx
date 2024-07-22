ff7cc31a10d88199d7bf9211d9ccbd531c56845c
Arina0212 <Shubina.Arina@urfu.me>
2024-07-23 01:20:54 +0500

    Fixes connection ResultPage and RoomPage with backend

--- frontend/src/components/Timer.tsx
+++ frontend/src/components/Timer.tsx
@@ -1,13 +1,14 @@
 import { Box, Button, CardMedia, Container, Dialog, DialogContent, DialogContentText, DialogTitle, Stack, TextField, Typography } from "@mui/material";
-import { useEffect, useState } from "react"
+import { FormEvent, useEffect, useState } from "react"
 import { useSelector } from "react-redux";
-import { selectGame } from "../store/gameSlice";
+import { selectGame, getGameByCode } from "../store/gameSlice";
 import Question from "../utils/types/Question";
 import { useNavigate, useParams } from "react-router-dom";
 import { patchAnswer } from "../store/playerSlice";
 import { useMediaMatch } from "../hooks/useMobileMatch";
 import { useAppDispatch } from "../store/storeHooks";
 import Player from "../utils/types/Player";
+
 type QwestionProps={
   qwestion:Question;
   player:Player
@@ -15,7 +16,7 @@ type QwestionProps={
 let count: number = 1
 let COUNTDOWN_INITIAL_TIME_IN_SECONDS = 30
 export let gameQwestion: Question
-export default function Timer( {qwestion, player}:QwestionProps) {
+export default function Timer( {qwestion, player}: QwestionProps) {
   const navigate = useNavigate();
   const urlParams = useParams();
   const [secondsAmount, setSecondsAmount] = useState(COUNTDOWN_INITIAL_TIME_IN_SECONDS)
@@ -26,18 +27,26 @@ export default function Timer( {qwestion, player}:QwestionProps) {
   const {isMobile} = useMediaMatch();
   const dispatch = useAppDispatch();
   //qwestion = game.questions[count-1]
-  useEffect(() => {    
+  useEffect(() => {
     if (secondsAmount == 0) {
       setOpen(true);
       setTimeout(() => {
         count = count + 1;
         setOpen(false);
         setSecondsAmount(30);
+        //dispatch(getQuestion(qwestion.id + 1));
+        //dispatch(getImageQuestion(qwestion.id + 1));
+        dispatch(getGameByCode(((game.code))));
         //qwestion = game.questions[count-1];
-        console.log('timout',count, qwestion);
+        //console.log('timout',count, qwestion.id);
         if (count===11){
-          navigate(`/results/${Number(urlParams.id)}`)
+          navigate(`/results/${urlParams.id}`)
         }
+        let answerButton = document.getElementById("answerButton");
+        if(answerButton !== null){
+          answerButton.style.display = "";
+          answerButton.style.pointerEvents = "auto";
+        } 
       }, 10000)
       return;
     }
@@ -46,27 +55,40 @@ export default function Timer( {qwestion, player}:QwestionProps) {
       setSecondsAmount(state => state - 1);
     }, 1000)
   }, [secondsAmount]);
+  // useEffect(() => {
+  //   dispatch(getImageQuestion(qwestion.id));
+  // }, [dispatch, qwestion.id]);
+  useEffect(() => {
+    dispatch(getGameByCode(game.code));
+}, [dispatch, game.code]);
+// for( let i=0; i<=10; i++){
+//   dispatch(getImageQuestion(game.questions[i].id));
+// }
 
-  
+  //let q = dispatch(getQuestion(qwestion.id));
   qwestion = game.questions[count-1];
-  console.log(count, qwestion)
+  //console.log(count, q)
   const minutes = Math.floor(secondsAmount / 60);
   const seconds = secondsAmount % 60;
   //qwestion = game.questions[count-1];
   const handleClose = () => {
     setOpen(false);
   };
-  const handleClickOpen = () => {
-    setOpen(true);
-  };
-
-  const handleSubmit = () => {
+  const handleSubmit = (evt: FormEvent<HTMLFormElement>) => {
+    evt.preventDefault();
     dispatch(patchAnswer(
       {player_id: player.id,
       player_answer: filmAnswer,
       question_id: qwestion.id,
       seconds: seconds}
-    ))   
+    ))
+    let answerButton = document.getElementById("answerButton");
+    if(answerButton !== null){
+      answerButton.style.display = "none";
+      answerButton.style.pointerEvents = "none";
+
+    }
+    setFilm('')
   }
 
 
@@ -100,7 +122,7 @@ export default function Timer( {qwestion, player}:QwestionProps) {
   }}
   return (
     <>
-    <Dialog
+      <Dialog
         open={open}
         onClose={handleClose}
         aria-labelledby="alert-dialog-title"
@@ -114,52 +136,48 @@ export default function Timer( {qwestion, player}:QwestionProps) {
           </DialogContentText>
         </DialogContent>
       </Dialog>
-      <Box>
-            <Button
-              variant='text'
-              onClick={handleClickOpen}>
-                Показать правильный ответ
-            </Button>
-          </Box>
-    <Box sx={{display:'flex', justifyContent:'center'}}>
-      <Box sx={{fontSize:'2.5vh', fontWeight:500, border:'3px solid #F49C1E', color:'#F49C1E', borderRadius:'30px', width:'15vh'}}>
-        <span>{String(minutes).padStart(2, '0')}</span>
-        <span>:</span>
-        <span>{String(seconds).padStart(2, '0')}</span>
-      </Box>
-    </Box>
-    <Box sx={{display: 'flex', flexDirection: 'column', alignItems:'center'}}>
-          <Typography>Название какого фильма изобразил ИИ?</Typography>
-          <Typography>{qwestion.answer}</Typography>
-          <CardMedia sx={{height:'40vh', width:'40vh', borderRadius:'2vh', marginBottom:'2vh'}}
-            id='img1'
-            component="img"
-            alt="Сгенерированное изображение"
-            //image={game.questions[0].image}
-            image="https://images.unsplash.com/photo-1518756131217-31eb79b20e8f"
-            onMouseOver={ZoomImg}
-            onMouseOut={UnzoomImg}>
-          </CardMedia>
-          <Typography>Раунд {count} из 10</Typography>
+      <Box sx={{display:'flex', justifyContent:'center'}}>
+        <Box sx={{fontSize:'2.5vh', fontWeight:500, border:'3px solid #F49C1E', color:'#F49C1E', borderRadius:'30px', width:'15vh'}}>
+          <span>{String(minutes).padStart(2, '0')}</span>
+          <span>:</span>
+          <span>{String(seconds).padStart(2, '0')}</span>
         </Box>
-        <Container maxWidth='md' sx={{position:'absolute', bottom:'1vh', left:'50%', marginRight:'-50%', transform:'translate(-50%, -50%)', alignItems:'center', margin:'0 auto', display: 'flex', flexDirection: 'column', paddingTop:'10px', gap: 1}}>
+      </Box>
+
+      <Box sx={{display: 'flex', flexDirection: 'column', alignItems:'center'}}>
+        <Typography>Название какого фильма изобразил ИИ?</Typography>
+        {/* <Typography>{qwestion.answer}</Typography> */}
+        <CardMedia sx={{height:'40vh', width:'40vh', borderRadius:'2vh', marginBottom:'2vh'}}
+          id='img1'
+          component="img"
+          alt="Сгенерированное изображение"
+          image={`data:image/png;base64,${qwestion.image}`}
+          //image="https://images.unsplash.com/photo-1518756131217-31eb79b20e8f"
+          onMouseOver={ZoomImg}
+          onMouseOut={UnzoomImg}>
+        </CardMedia>
+        <Typography>Раунд {count} из 10</Typography>
+      </Box>
+      <Container maxWidth='md' sx={{position:'absolute', bottom:'1vh', left:'50%', marginRight:'-50%', transform:'translate(-50%, -50%)', alignItems:'center', margin:'0 auto', display: 'flex', flexDirection: 'column', paddingTop:'10px', gap: 1}}>
+        <form onSubmit={handleSubmit}>
           <Stack direction={isMobile ? 'column' : 'row'} sx={{display: 'flex', alignItems:'center', gap: 1}}>
             <TextField 
               id='answer' 
               label='Введите Ваш ответ' 
               variant='filled' 
               required
-              color='primary' 
+              color='primary'
               onChange={onFilmNameChange}
+              value={filmAnswer}
             />
             <Button
               id='answerButton'
               variant='contained'
-              type='submit'
-              onClick={handleSubmit}>
+              type='submit'>
                 Ответить
             </Button>
           </Stack>
+        </form>
       </Container>
     </>
   )
--- frontend/src/views/ResultsPage.tsx
+++ frontend/src/views/ResultsPage.tsx
@@ -1,18 +1,45 @@
 import { Avatar, Box, Button, Container, List, Stack, Typography } from "@mui/material";
 import PersonIcon from '@mui/icons-material/Person';
-import { useNavigate } from "react-router-dom";
-import { nanoid } from "nanoid";
+import { useNavigate} from "react-router-dom";
 import { useMediaMatch } from '../hooks/useMobileMatch';
 import { useSelector } from "react-redux";
-import { selectPlayer } from "../store/playerSlice";
+import { getGameByCode, selectGame } from "../store/gameSlice";
+import { useEffect } from "react";
+import { useAppDispatch } from "../store/storeHooks";
+import Player from "../utils/types/Player";
 
 
 export default function ResultsPage(){
     const navigate = useNavigate();
     const {isMobile} = useMediaMatch();
-    const player = useSelector(selectPlayer)
-    console.log(player)
-      
+    const dispatch = useAppDispatch();
+    const {game} = useSelector(selectGame)
+
+    useEffect(() => {
+        dispatch(getGameByCode( game.code));
+    }, [dispatch,  game.code]);
+    console.log(game)
+    const onGameCreate = () => {
+        navigate(`/`);
+        //dispatch(signUp(true));
+        //dispatch(postCreateGame());
+        // if(game.id > 0) {
+        //     navigate(`/lobby/${game.id}`);
+        //     navigate(`/`);
+        // }
+    };
+    let players = game.players
+    let firstPlace
+    let playersFiltered = players.sort((a, b) => Number(a.score) - Number(b.score)).reverse();
+    if(playersFiltered.length !== 0 ){
+        if(playersFiltered.length > 1){
+            firstPlace = playersFiltered.splice(0,1)
+            console.log(firstPlace)
+            console.log(playersFiltered)
+        }
+        console.log(playersFiltered)
+    }
+    console.log(firstPlace)
     return(
         <Container maxWidth='lg' sx={{padding: '1.3vh', borderRadius: 5, display: 'flex', flexDirection: 'column', flexWrap: 'wrap'}}>
           <Container maxWidth='md' sx={{display: 'flex', padding: '0vw', flexDirection: 'row',justifyContent:'center', alignItems: 'center', gap:'2px'}}>
@@ -21,137 +48,48 @@ export default function ResultsPage(){
                 <Box width={'100%'} display={isMobile ? 'none' : 'flex'} >
                     <Typography sx={{textAlign:'left', padding:'0.1vh 2vh', marginY:'1vh', border:'6px solid #FDD59C', borderRadius:'41px', background:'#FDD59C', color:'black'}}>Победитель:</Typography>
                 </Box>
-                <Stack direction={isMobile ? 'column' : 'row'}  sx={{display:'flex', alignItems:'center', paddingY:'1vh', margin:'0.9vh', border:'6px solid #FDD59C', borderRadius:'41px', background:'#FDD59C', width:'100%'}}>
-                    <Avatar alt='avatar 1' variant='circular' sx={{ width: '15vh', height: '15vh'}} >
-                        <PersonIcon sx={{color: 'black', width: '10vh', height: '10vh'}}/>
-                    </Avatar>
-                    <Box alignItems={isMobile ? 'center' : 'flex-start'} sx={{margin:'1.6vw', display:'flex', flexDirection:'column', gap:'3vh'}}>
-                            <Typography style={{fontWeight:'500', fontSize:'3vh', color:'#C94F48'}}>Екатерина</Typography>
-                            {/* <CardMedia 
-                                component="img"
-                                alt="Медаль" 
-                                image="/assets/medal.png"> 
-                            </CardMedia> */}
-                        <Typography style={{fontWeight:'500', fontSize:'2vh', color:'#C94F48'}}>Итоговые баллы: 2500</Typography>
-                    </Box>
-                </Stack>
-                <List style={{maxHeight: '30vh', width:'100%', overflowY: 'scroll', overflowX:'hidden', display:'flex', flexDirection:"column",  flexWrap:"nowrap", justifyContent:'space-between', marginBottom:'1.6vw', rowGap:'1vh'  }}>
-                    <Box sx={{width:'99%', display: 'flex', justifyContent:'space-between', flexDirection: 'row', columnGap:'6vw',  alignItems: 'center', paddingBottom:'1.1vw', paddingRight:'1vh'}}>
+                {(playersFiltered.length === 1)?
+                    <Stack direction={isMobile ? 'column' : 'row'}  sx={{display:'flex', alignItems:'center', paddingY:'1vh', margin:'0.9vh', border:'6px solid #FDD59C', borderRadius:'41px', background:'#FDD59C', width:'100%'}}>
+                        <Avatar alt='avatar 1' variant='circular'  sx={{ width: '15vh', height: '15vh'}} >
+                            <PersonIcon sx={{color: 'black', width: '10vh', height: '10vh'}}/>
+                        </Avatar>
+                        <Box alignItems={isMobile ? 'center' : 'flex-start'} sx={{margin:'1.6vw', display:'flex', flexDirection:'column', gap:'3vh'}}>
+                                <Typography style={{fontWeight:'500', fontSize:'3vh', color:'#C94F48'}}>{playersFiltered[0].name}</Typography>
+                            <Typography style={{fontWeight:'500', fontSize:'2vh', color:'#C94F48'}}>Итоговые баллы: {playersFiltered[0].score}</Typography>
+                        </Box>
+                    </Stack>
+                    :<>
+                        {(firstPlace!==undefined)?
+                        <Box><Stack direction={isMobile ? 'column' : 'row'}  sx={{display:'flex', alignItems:'center', paddingY:'1vh', margin:'0.9vh', border:'6px solid #FDD59C', borderRadius:'41px', background:'#FDD59C', width:'100%'}}>
+                        <Avatar alt='avatar 1' variant='circular' src={firstPlace[0].avatar} sx={{ width: '15vh', height: '15vh'}} >
+                            <PersonIcon sx={{color: 'black', width: '10vh', height: '10vh'}}/>
+                        </Avatar>
+                        <Box alignItems={isMobile ? 'center' : 'flex-start'} sx={{margin:'1.6vw', display:'flex', flexDirection:'column', gap:'3vh'}}>
+                                <Typography style={{fontWeight:'500', fontSize:'3vh', color:'#C94F48'}}>{firstPlace[0].name}</Typography>
+                            <Typography style={{fontWeight:'500', fontSize:'2vh', color:'#C94F48'}}>Итоговые баллы: {firstPlace[0].score}</Typography>
+                        </Box>
+                    </Stack>
+                    <List style={{maxHeight: '30vh', width:'100%', overflowY: 'scroll', overflowX:'hidden', display:'flex', flexDirection:"column",  flexWrap:"nowrap", justifyContent:'space-between', marginBottom:'1.6vw', rowGap:'1vh'  }}>
+                    {(playersFiltered.map((player: Player, place: number)=>
+                        <Box sx={{width:'99%', display: 'flex', justifyContent:'space-between', flexDirection: 'row', columnGap:'6vw',  alignItems: 'center', paddingBottom:'1.1vw', paddingRight:'1vh'}}>
                         <Box sx={{flexDirection: 'row',display: 'flex', alignItems:'center'}}>
-                            <Typography sx={{width:'11vh', paddingRight:'5vh'}}>2 место</Typography>
+                            <Typography sx={{width:'11vh', paddingRight:'3vh'}}>{place+2} место</Typography>
                                 <Box sx={{flexDirection: 'row',display: 'flex', alignItems:'center'}}>
-                                    <Avatar alt='avatar 1' variant='circular' sx={{ width: '5vh', height: '5vh', marginRight:'1vh'}} >
+                                    <Avatar alt='avatar 1' variant='circular' src={player.avatar} sx={{ width: '5vh', height: '5vh', marginRight:'1vh'}} >
                                         <PersonIcon sx={{color: 'black', width: '3vh', height: '3vh'}}/>
                                     </Avatar>
-                                    <Typography>Александр</Typography>
+                                    <Typography>{player.name}</Typography>
                                 </Box>
                         </Box>
-                        <Typography>2500</Typography>
+                        <Typography>{player.score}</Typography>
                     </Box>
-                    <Box sx={{width:'99%', display: 'flex', justifyContent:'space-between', flexDirection: 'row', columnGap:'6vw',  alignItems: 'center', paddingBottom:'1.1vw', paddingRight:'1vh'}}>
-                    <Box sx={{flexDirection: 'row',display: 'flex', alignItems:'center'}}>
-                        <Typography sx={{width:'11vh', paddingRight:'5vh'}}>3 место</Typography>
-                        <Box sx={{flexDirection: 'row',display: 'flex', alignItems:'center'}}>
-                            <Avatar alt='avatar 1' variant='circular' sx={{ width: '5vh', height: '5vh', marginRight:'1vh'}} >
-                                <PersonIcon sx={{color: 'black', width: '3vh', height: '3vh'}}/>
-                            </Avatar>
-                            
-                            <Typography>Яков</Typography>
-                        </Box>
-                        </Box>
-                        <Typography>2000</Typography>
-                    </Box>
-                    <Box sx={{width:'99%', display: 'flex', justifyContent:'space-between', flexDirection: 'row', columnGap:'6vw',  alignItems: 'center', paddingBottom:'1.1vw', paddingRight:'1vh'}}>
-                    <Box sx={{flexDirection: 'row',display: 'flex', alignItems:'center'}}>
-                        <Typography sx={{width:'11vh', paddingRight:'5vh'}}>4 место</Typography>
-                        <Box sx={{flexDirection: 'row',display: 'flex', alignItems:'center'}}>
-                            <Avatar alt='avatar 1' variant='circular' sx={{ width: '5vh', height: '5vh', marginRight:'1vh'}} >
-                                <PersonIcon sx={{color: 'black', width: '3vh', height: '3vh'}}/>
-                            </Avatar>
-                            
-                            <Typography>Яков</Typography>
-                        </Box>
-                        </Box>
-                        <Typography>800</Typography>
-                    </Box>
-                    <Box sx={{width:'99%', display: 'flex', justifyContent:'space-between', flexDirection: 'row', columnGap:'6vw',  alignItems: 'center', paddingBottom:'1.1vw', paddingRight:'1vh'}}>
-                    <Box sx={{flexDirection: 'row',display: 'flex', alignItems:'center'}}>
-                        <Typography sx={{width:'11vh', paddingRight:'5vh'}}>5 место</Typography>
-                        <Box sx={{flexDirection: 'row',display: 'flex', alignItems:'center'}}>
-                            <Avatar alt='avatar 1' variant='circular' sx={{ width: '5vh', height: '5vh', marginRight:'1vh'}} >
-                                <PersonIcon sx={{color: 'black', width: '3vh', height: '3vh'}}/>
-                            </Avatar>
-                            
-                            <Typography>Яков</Typography>
-                        </Box>
-                        </Box>
-                        <Typography>500</Typography>
-                    </Box>
-                    <Box sx={{width:'99%', display: 'flex', justifyContent:'space-between', flexDirection: 'row', columnGap:'6vw',  alignItems: 'center', paddingBottom:'1.1vw', paddingRight:'1vh'}}>
-                    <Box sx={{flexDirection: 'row',display: 'flex', alignItems:'center'}}>
-                        <Typography sx={{width:'11vh', paddingRight:'5vh'}}>6 место</Typography>
-                        <Box sx={{flexDirection: 'row',display: 'flex', alignItems:'center'}}>
-                            <Avatar alt='avatar 1' variant='circular' sx={{ width: '5vh', height: '5vh', marginRight:'1vh'}} >
-                                <PersonIcon sx={{color: 'black', width: '3vh', height: '3vh'}}/>
-                            </Avatar>
-                            
-                            <Typography>Ян</Typography>
-                        </Box>
-                        </Box>
-                        <Typography>800</Typography>
-                    </Box>
-                    <Box sx={{width:'99%', display: 'flex', justifyContent:'space-between', flexDirection: 'row', columnGap:'6vw',  alignItems: 'center', paddingBottom:'1.1vw', paddingRight:'1vh'}}>
-                    <Box sx={{flexDirection: 'row',display: 'flex', alignItems:'center'}}>
-                        <Typography sx={{width:'11vh', paddingRight:'5vh'}}>7 место</Typography>
-                        <Box sx={{flexDirection: 'row',display: 'flex', alignItems:'center'}}>
-                            <Avatar alt='avatar 1' variant='circular' sx={{ width: '5vh', height: '5vh', marginRight:'1vh'}} >
-                                <PersonIcon sx={{color: 'black', width: '3vh', height: '3vh'}}/>
-                            </Avatar>
-                            
-                            <Typography>Евгения</Typography>
-                        </Box>
-                        </Box>
-                        <Typography>800</Typography>
-                    </Box>
-                    <Box sx={{width:'99%', display: 'flex', justifyContent:'space-between', flexDirection: 'row', columnGap:'6vw',  alignItems: 'center', paddingBottom:'1.1vw', paddingRight:'1vh'}}>
-                    <Box sx={{flexDirection: 'row',display: 'flex', alignItems:'center'}}>
-                        <Typography sx={{width:'11vh', paddingRight:'5vh'}}>8 место</Typography>
-                        <Box sx={{flexDirection: 'row',display: 'flex', alignItems:'center'}}>
-                            <Avatar alt='avatar 1' variant='circular' sx={{ width: '5vh', height: '5vh', marginRight:'1vh'}} >
-                                <PersonIcon sx={{color: 'black', width: '3vh', height: '3vh'}}/>
-                            </Avatar>
-                            
-                            <Typography>Александр</Typography>
-                        </Box>
-                        </Box>
-                        <Typography>0</Typography>
-                    </Box>
-                    <Box sx={{width:'99%', display: 'flex', justifyContent:'space-between', flexDirection: 'row', columnGap:'6vw',  alignItems: 'center', paddingBottom:'1.1vw', paddingRight:'1vh'}}>
-                    <Box sx={{flexDirection: 'row',display: 'flex', alignItems:'center'}}>
-                        <Typography sx={{width:'11vh', paddingRight:'5vh'}}>9 место</Typography>
-                        <Box sx={{flexDirection: 'row',display: 'flex', alignItems:'center'}}>
-                            <Avatar alt='avatar 1' variant='circular' sx={{ width: '5vh', height: '5vh', marginRight:'1vh'}} >
-                                <PersonIcon sx={{color: 'black', width: '3vh', height: '3vh'}}/>
-                            </Avatar>
-                            
-                            <Typography>Яков</Typography>
-                        </Box>
-                        </Box>
-                        <Typography>0</Typography>
-                    </Box>
-                    <Box sx={{width:'99%', display: 'flex', justifyContent:'space-between', flexDirection: 'row', columnGap:'6vw',  alignItems: 'center', paddingBottom:'1.1vw', paddingRight:'1vh'}}>
-                    <Box sx={{flexDirection: 'row',display: 'flex', alignItems:'center'}}>
-                        <Typography sx={{width:'11vh', paddingRight:'5vh'}}>10 место</Typography>
-                        <Box sx={{flexDirection: 'row',display: 'flex', alignItems:'center'}}>
-                            <Avatar alt='avatar 1' variant='circular' sx={{ width: '5vh', height: '5vh', marginRight:'1vh'}} >
-                                <PersonIcon sx={{color: 'black', width: '3vh', height: '3vh'}}/>
-                            </Avatar>
-                            <Typography>Яков</Typography>
-                        </Box>
-                        </Box>
-                        <Typography>800</Typography>
-                    </Box>
-                </List>
+                    ))}
+                    </List>
+                        </Box>:<Typography>Игроки не найдены</Typography>}
+                    </>
+                    }
+                    
+                
                 <Container maxWidth='md' sx={{position:'absolute', bottom:'1vh', left:'50%', marginRight:'-50%', transform:'translate(-50%, -50%)', alignItems:'center', margin:'0 auto', display: 'flex', flexDirection: 'row', justifyContent:'space-around', paddingBottom:'10px', gap: 1}}>
                     <Box display={isMobile ? 'none' : 'flex'}>
                         <Button
@@ -163,7 +101,7 @@ export default function ResultsPage(){
                     </Box>
                     <Button 
                         variant='contained'
-                        onClick={() => navigate(`/lobby/${nanoid()}`)}>
+                        onClick={onGameCreate}>
                             Играть снова
                     </Button>
                 </Container>
